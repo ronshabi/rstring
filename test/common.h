33429bc2a49d037b92c651930c4d7a8d40b9a9c4
Ron Shabi <ron@ronsh.net>
2025-05-11 01:13:47 +0300

    Tests: add common testlib header test.h
    To not repeat myself, this header will contain some common function
    every test uses.
    
    Signed-off-by: Ron Shabi <ron@ronsh.net>

---
+++ test/common.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+// Common function every test is most likely to use
+// clang-tidy: nolint (testname and fmt are both char* arguments)
+__attribute__((format(printf, 2, 3), noreturn))
+static inline void test_fail (const char *testname, const char *fmt, ...) // NOLINT
+{
+    va_list ap;
+    va_start(ap, fmt);
+
+    printf("[FAIL: %s]  ", testname);
+
+    vprintf(fmt, ap);
+
+    va_end(ap);
+
+    printf("\n");
+    exit(1); // NOLINT (exit is not thread safe)
+}
