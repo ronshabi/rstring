d1b20774f783119c76c1cc5c63478b886775544d
Ron Shabi <ron@ronsh.net>
2025-05-11 22:31:42 +0300

    Tests/t2: use streamlined exit procedure, add cleanup
    Signed-off-by: Ron Shabi <ron@ronsh.net>

--- test/t2-find.c
+++ test/t2-find.c
@@ -1,7 +1,13 @@
+#define _GNU_SOURCE /* strcasestr */
+
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <stdbool.h>
+
 #include <rstring/rstring.h>
+
+#include "common.h"
 
 struct rstring
 init_test(const char *test_name, const char *str)
@@ -29,12 +35,15 @@ find_first_byte_test(const char *str, uint8_t byte, size_t from, size_t result)
 
     if (result != given_result)
     {
-        printf("Test find_first_byte_test failed:\n"
-               "In string '%s', find byte 0x%02x, from offset %zu\n"
-               "Result: %zu\n"
-               "Expected: %zu\n",
-               str, byte, from, given_result, result);
-        exit(1);
+        test_fail(__FUNCTION__,
+                  "In string '%s', find byte 0x%02x, from offset %zu\n"
+                  "Result: %zu\n"
+                  "Expected: %zu\n",
+                  str,
+                  byte,
+                  from,
+                  given_result,
+                  result);
     }
 
     rstring_free(&rs);
@@ -49,15 +58,68 @@ find_last_byte_test(const char *str, uint8_t byte, size_t result)
 
     if (result != given_result)
     {
-        printf("Test find_last_byte_test failed:\n"
-               "In string '%s', find byte 0x%02x\n"
-               "Result: %zu\n"
-               "Expected: %zu\n",
-               str, byte, given_result, result);
-        exit(1);
+        test_fail(__FUNCTION__,
+                  "In string '%s', find byte 0x%02x\n"
+                  "Result: %zu\n"
+                  "Expected: %zu\n",
+                  str,
+                  byte,
+                  given_result,
+                  result);
     }
 
     rstring_free(&rs);
+}
+
+void
+find_first_test(const char *haystack, const char *needle, size_t from,
+                bool ignore_case, size_t result)
+{
+    // Here I'm gonna test both the regular and the 'str' variant of these
+    // functions. They should return the same results irrelevent to how what is
+    // the type of the second argument.
+
+    struct rstring rs_haystack =
+        init_test("find_first_test (haystack)", haystack);
+    struct rstring rs_needle = init_test("find_first_test (needle)", needle);
+
+    size_t res_rs_rs  = 0;
+    size_t res_rs_str = 0;
+
+    if (ignore_case)
+    {
+        res_rs_rs =
+            rstring_find_first_ignore_case(&rs_haystack, &rs_needle, from);
+        res_rs_str =
+            rstring_find_first_str_ignore_case(&rs_haystack, needle, from);
+    }
+    else
+    {
+        res_rs_rs  = rstring_find_first(&rs_haystack, &rs_needle, from);
+        res_rs_str = rstring_find_first_str(&rs_haystack, needle, from);
+    }
+
+    if (res_rs_rs != result || res_rs_str != result)
+    {
+        test_fail(__FUNCTION__,
+                  "\n\t> haystack: '%s'\n"
+                  "\t> needle: '%s'\n"
+                  "\t> from: %zu\n"
+                  "\t> ignore case?: %s\n"
+                  "\t> expected: %zu\n"
+                  "\t> result of regular variant: %zu\n"
+                  "\t> result of str variant: %zu\n",
+                  haystack,
+                  needle,
+                  from,
+                  ignore_case ? "true" : "false",
+                  result,
+                  res_rs_rs,
+                  res_rs_str);
+    }
+
+    rstring_free(&rs_haystack);
+    rstring_free(&rs_needle);
 }
 
 int
